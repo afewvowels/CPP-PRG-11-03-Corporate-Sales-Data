c88dbbd5f9db86a82fd1c9948bdc9a87081a1909
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-06 17:44:19 -0500

    Prompt written.

--- CPP-PRG-11-03-Corporate-Sales-Data/main.cpp
+++ CPP-PRG-11-03-Corporate-Sales-Data/main.cpp
@@ -5,11 +5,30 @@
 //  Created by Keith Smith on 11/6/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that uses a structure to store the following data on a company division:
+//
+//      Division Name (such as East, West, North, or South)
+//      First-Quarter Sales
+//      Second-Quarter Sales
+//      Third-Quarter Sales
+//      Fourth-Quarter Sales
+//      Total Annual Sales
+//      Average Quarterly Sales
+//
+//  The program should use four variables of this structure. Each variable should represent
+//  one of the following corporate divisions: East, West, North, and South. The user
+//  should be asked for the four quarters' sales figures for each division. Each division's
+//  total and average sales should be calculated and stored in the appropriate member of
+//  each structure variable. These figures should then be displayed on the screen.
+//
+//  Input Validation: Do not accept negative numbers for any sales figures.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    
+    
     return 0;
 }
